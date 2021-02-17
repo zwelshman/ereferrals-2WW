8009cd4cd7d547687ae57f6ac194ae2abd97068f
zwelshman <zmswelshman@gmail.com>
2021-02-17 15:10:47 +0000

    update

--- stream_app.py
+++ stream_app.py
@@ -6,26 +6,29 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import time
 
+from src.dataframe_manipulation import filtered_dataframe
+
 matplotlib.style.use("ggplot")
 # plt.rcParams["figure.figsize"] = [16, 6]
 
 st.title("Open Data Extracted From NHS e-referrals.")
 
-rad = st.sidebar.radio("Navigation", ["Two Week Wait Analysis", "About me"])
+rad = st.sidebar.radio(
+    "Navigation",
+    ["Analysis for Two Week Wait", "Advanced Analytics", "About me"],
+)
 
 if rad == "About me":
     "Markdown: #Work in progress"
 
-if rad == "Two Week Wait Analysis":
+if rad == "Analysis for Two Week Wait":
 
     @st.cache(allow_output_mutation=True)
     def load_data():
         data = pd.read_csv("./app_data/all.csv")
         return data
 
-    with st.spinner(
-        "Wait for it... Just loading the data "
-    ):
+    with st.spinner("Wait for it... Just loading the data"):
 
         my_bar = st.progress(0)
         for percent_complete in range(100):
@@ -33,7 +36,9 @@ if rad == "Two Week Wait Analysis":
             df.loc[(df.day_of_year > 358) & (df.week_of_year == 1), "week_of_year"] = 52
             my_bar.progress(percent_complete + 1)
 
-        st.success("Done, your data has loaded! Don't forget to check the correct filters are selected.")
+        st.success(
+            "Done, your data has loaded! Don't forget to check the correct filters are selected."
+        )
 
     # option = st.sidebar.selectbox(
     #     'Which number do you like best?',
@@ -41,34 +46,9 @@ if rad == "Two Week Wait Analysis":
 
     # 'You selected:', option
 
-    # Create a list of possible values and multiselect menu with them in it.
-    CCG = list(df["CCG_Name"].unique())
-    CCG_SELECTED = st.multiselect("Select CCG", CCG, default=['NHS LEEDS CCG', 'NHS ROTHERHAM CCG'])
-    # Mask to filter dataframe
-    mask_CCG = df["CCG_Name"].isin(CCG_SELECTED)
-    df = df[mask_CCG]
-    # st.write('You selected:', CCG_SELECTED)
-
-    # Create a list of possible values and multiselect menu with them in it.
-    Specialty = list(df["Specialty"].unique())
-    Specialty_SELECTED = st.multiselect("Select Specialty", Specialty, default=['2WW'])
-    # Mask to filter dataframe
-    mask_Specialty = df["Specialty"].isin(Specialty_SELECTED)
-    df = df[mask_Specialty]
-
-    # Create a list of possible values and multiselect menu with them in it.
-    Priority = list(df["Priority"].unique())
-    Priority_SELECTED = st.multiselect("Select Priority", Priority, default=['2 Week Wait', 'Urgent', 'Routine'])
-    # Mask to filter dataframe
-    mask_Priority = df["Priority"].isin(Priority_SELECTED)
-    df = df[mask_Priority]
-    # st.write('You selected:', Specialty_SELECTED)
-
-    # months_values = list(df['month'].unique())
-    # Months_SELECTED = st.slider('Select a range of values for months', 0, 12, (0, 12))
-    # mask_Months = df['month'].isin(Months_SELECTED)
-    # df = df[mask_Months]
-    # st.write('Values:', Months_SELECTED)
+    df = filtered_dataframe(df, 'CCG_Name', default=["NHS LEEDS CCG", "NHS ROTHERHAM CCG"])
+    df = filtered_dataframe(df, 'Specialty', default=["2WW"])
+    df = filtered_dataframe(df, 'Priority', default=["2 Week Wait", "Urgent", "Routine"])
 
     st.subheader("Raw Weekly Referral 2WW data")
     st.write(df)
@@ -115,3 +95,6 @@ if rad == "Two Week Wait Analysis":
     st.subheader("Weekly Referral Aggregate 2WW data")
     st.write(testing_group_week)
 
+
+if rad == "Advanced Analytics":
+    "Markdown: #Work in progress"
