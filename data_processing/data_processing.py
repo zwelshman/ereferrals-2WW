bc5545b9d248360232f42066bf4efbcf444fb4d0
zwelshman <zmswelshman@gmail.com>
2021-02-18 17:25:02 +0000

    added_merge_csv and create if not there

--- data_processing/data_processing.py
+++ data_processing/data_processing.py
@@ -29,3 +29,12 @@ def format_dataframe(df):
 # Place in new folder
 def to_folder_csv(df, location):
     df.to_csv(location, index=False)
+
+print("Merging files")
+merge_cvs()
+print("Removing headers")
+df_headers_removed = remove_headers("app_data/Referrals_csv_files/", "merge.csv")
+print("Data analysis data")
+data_to_analyse = format_dataframe(df_headers_removed)
+print("Placing in folder")
+to_folder_csv(data_to_analyse, "app_data/all.csv")
---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+FILE=app_data/Referrals_csv_files/all.csv
+
+if test -f "$FILE"
+then
+    streamlit run stream_app.py
+else
+    python ./data_processing/data_processing.py
+    streamlit run stream_app.py
+fi
