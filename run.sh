9ece3dc5a5ce89ad97075d80cc733ecaa0e9b1dc
zwelshman <zmswelshman@gmail.com>
2021-02-18 19:13:08 +0000

    deleted run sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-FILE=app_data/Referrals_csv_files/all.csv
-
-if test -f "$FILE"
-then
-    streamlit run stream_app.py
-else
-    python ./data_processing/data_processing.py 
-    streamlit run stream_app.py
-fi
