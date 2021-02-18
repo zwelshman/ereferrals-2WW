b3651f31976141471dee088c43349747b95c640b
zwelshman <zmswelshman@gmail.com>
2021-02-18 18:27:09 +0000

    testing crappy sh file problems...

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ if test -f "$FILE"
 then
     streamlit run stream_app.py
 else
-    python ./data_processing/data_processing.py
+    python ./data_processing/data_processing.py 
     streamlit run stream_app.py
 fi
