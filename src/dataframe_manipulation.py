4ca07b07f94b8a2e817b4bfa76d16792f63deca8
zwelshman <zmswelshman@gmail.com>
2021-02-19 11:32:05 +0000

    added and all option

--- src/dataframe_manipulation.py
+++ src/dataframe_manipulation.py
@@ -2,17 +2,14 @@ import pandas as pd
 import streamlit as st
 
 def filtered_dataframe(df: pd.DataFrame, column: str, default: list):
-    Specialty = list(df[column].unique())
-    Specialty_SELECTED = st.multiselect(f"Select {column}", Specialty, default=default)
+    options = list(df[column].unique())
+    options.append("All")
+    value = st.multiselect(f"Select {column}", options, default=default)
+    
+    if "All" in value:
+        value = options
+
     # Mask to filter dataframe
-    mask = df[column].isin(Specialty_SELECTED)
+    mask = df[column].isin(value)
     df_out = df[mask]
     return df_out
-
-    # # Create a list of possible values and multiselect menu with them in it.
-    # CCG = list(df["CCG_Name"].unique())
-    # CCG_SELECTED = st.multiselect("Select CCG", CCG, default=["NHS LEEDS CCG", "NHS ROTHERHAM CCG"])
-    # # Mask to filter dataframe
-    # mask_CCG = df["CCG_Name"].isin(CCG_SELECTED)
-    # df = df[mask_CCG]
-    # # st.write('You selected:', CCG_SELECTED)
